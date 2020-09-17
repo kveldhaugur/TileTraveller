e3ef086aa025d857cb961d101cb2307242c63962
kveldhaugur <konrad20@ru.is>
2020-09-17 23:51:12 +0000

    Done

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,3 +1,47 @@
+# In this assignment, you work in groups of 2-3 students each and apply pair programing. Moreover, you need to use git during the development of your solution.
+
+# The assignment is to develop a computer game in which the player is located in a certain tile in a grid.  At each iteration, the program displays the directions for which there are adjacent tiles that the player can travel to.  
+
+# The program only displays text, so you don’t actually draw the tile grid, but the program should behave as if the player is in a 3x3 grid with open and closed walls as seen in the following image:
+
+# The player starts in tile (1,1). At the beginning, and after each move selected by the player, the program should print the player’s travel options. If there is an open wall in a direction, write that direction as a possible travel direction.
+
+# At each iteration, the player enters the first letter of the direction he/she wishes to travel, after which the player should be located in another tile and the options for the new tile are then printed out.
+
+# The player enters:
+
+#     n/N for north (up)
+#     e/E for east (right)
+#     s/S for south (down)
+#     w/W for west (left)
+
+# If the player enters an invalid direction, the program prints “Not a valid direction!” and allows the player to enter the direction again.
+
+# For example, in tile (1,1) it’s only possible to move north. In tile (1,2), the possible moves are north, east and south, and in tile (3,3) the valid moves are south and west.
+
+# Tile (3,1) is the victory location.  When the player enters this tile, the program should notify him/her of their victory and quit running.
+
+# Example run:
+
+# You can travel: (N)orth.
+# Direction: s
+# Not a valid direction!
+# You can travel: (N)orth.
+# Direction: n
+# You can travel: (N)orth or (E)ast or (S)outh.
+# Direction: N
+# You can travel: (E)ast or (S)outh.
+# Direction: w
+# Not a valid direction!
+# You can travel: (E)ast or (S)outh.
+# Direction: E
+# You can travel: (E)ast or (W)est.
+# Direction: e
+# You can travel: (S)outh or (W)est.
+# Direction: s
+# You can travel: (N)orth or (S)outh.
+# Direction: S
+# Victory!
 
 def move_north(x):
     (a, b) = x
@@ -64,28 +108,6 @@ possible_move = ""
 
 
 player_tile = (1, 1)
-
-#while player_tile != (3, 3):
-    #if 4 <= player_tile <= 0:
-        #print error
-        #continue
-
-    #while player_tile in (possible_x, possible_y):
-        #possible_move = possible_move.split(" ")
-        
-        #possible_move = " or ".join(possible_move)
-        #print("You can travel {}.".format(possible_move.split(" ")))
-        #player_input = input("Direction: ")
-        
-        #for player_tile is_not_wall
-
-        #if player_input == "n" and player_tile != (wall_1 or wall_2 or wall_3 or wall_4):
-            #player_tile = move_north(player_tile)
-            #possible_south = True
-            #possible_east = True
-            
-#print(player_tile)
-
 
 while player_tile in possible_tiles:
     possible_move = ""
