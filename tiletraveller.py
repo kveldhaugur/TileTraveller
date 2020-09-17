e9a88415bf3fede7adb307e4a91e1b73dee50a2c
kveldhaugur <konrad20@ru.is>
2020-09-17 09:47:36 +0000

    fds

---
+++ tiletraveller.py
@@ -0,0 +1,21 @@
+# búa til breytur fyrir tile_11...
+
+# búa til function sem fara north, south, east, west
+
+#veggir 
+
+def move_north():
+
+player = tile_1_1
+
+tile_1_1 = 1.1
+tile_1_2 = 1.2
+tile_1_3 = 1.3
+
+tile_2_1 = 2.1
+tile_2_2 = 2.2
+tile_2_3 = 2.3
+
+tile_3_1 = 3.1
+tile_3_2 = 3.2
+tile_3_3 = 3.3
